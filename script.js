b5996fd9349cd4ea93c52e28443124fa1fc39de1
Shahriar Fardows <shahriarfardows@gmail.com>
2023-09-28 02:31:24 +0600

    some changes

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-function uploadImage() {
-    const imageInput = document.getElementById("imageInput");
-    const imageTitle = document.getElementById("imageTitle").value;
-    const imageCaption = document.getElementById("imageCaption").value;
+document.addEventListener("DOMContentLoaded", () => {
+    const fileInput = document.getElementById("fileInput");
+    const uploadButton = document.getElementById("uploadButton");
+    const imageList = document.getElementById("imageList");
 
-    if (imageInput.files.length === 0) {
-        alert("Please select an image to upload.");
-        return;
+    uploadButton.addEventListener("click", () => {
+        fileInput.click();
+    });
+
+    fileInput.addEventListener("change", handleFileUpload);
+
+    function handleFileUpload(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                const imageUrl = e.target.result;
+                const title = prompt("Enter image title:");
+                const caption = prompt("Enter image caption:");
+
+                if (title || caption) {
+                    createImageElement(imageUrl, title, caption);
+                }
+            };
+
+            reader.readAsDataURL(file);
+        }
     }
 
-    const imageFile = imageInput.files[0];
-    const imageUrl = URL.createObjectURL(imageFile);
+    function createImageElement(url, title, caption) {
+        const imageContainer = document.createElement("div");
+        imageContainer.className = "image-container";
 
-    const imageContainer = document.createElement("div");
-    imageContainer.className = "image-container";
+        const img = document.createElement("img");
+        img.src = url;
 
-    const img = document.createElement("img");
-    img.src = imageUrl;
+        const captionElement = document.createElement("div");
+        captionElement.className = "caption";
+        captionElement.textContent = `Title: ${title || 'N/A'} | Caption: ${caption || 'N/A'}`;
 
-    const caption = document.createElement("p");
-    caption.textContent = imageCaption;
-
-    const downloadLink = document.getElementById("downloadLink");
-    downloadLink.href = imageUrl;
-    downloadLink.download = imageTitle || "image";
-
-    imageContainer.appendChild(img);
-    imageContainer.appendChild(caption);
-
-    const imageGallery = document.getElementById("imageGallery");
-    imageGallery.appendChild(imageContainer);
-
-    imageInput.value = "";
-    document.getElementById("imageTitle").value = "";
-    document.getElementById("imageCaption").value = "";
-    downloadLink.style.display = "block";
-}
+        imageContainer.appendChild(img);
+        imageContainer.appendChild(captionElement);
+        imageList.appendChild(imageContainer);
+    }
+});
